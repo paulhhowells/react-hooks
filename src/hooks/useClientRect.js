a3ae3c9629fead142922c40df4ca63ad6df8ae9e
Paul H Howells <p.h.howells@gmail.com>
2021-01-18 23:17:50 +0000

    feat: add useClientRect hook

---
+++ src/hooks/useClientRect.js
@@ -0,0 +1,12 @@
+import {useCallback, useState} from 'react';
+
+export default function useClientRect () {
+  const [rect, setRect] = useState(null);
+  const ref = useCallback(node => {
+    if (node !== null) {
+      setRect(node.getBoundingClientRect());
+    }
+	}, []);
+
+  return [rect, ref];
+}
