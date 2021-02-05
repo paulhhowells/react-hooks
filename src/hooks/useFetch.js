c3db12b85e4251d7ca2c9eb34d092b19d82c7c1b
Paul H Howells <p.h.howells@gmail.com>
2021-02-05 21:36:39 +0000

    feat: add useFetch hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+
+const defaultConfig = { method: 'GET' };
+
+export const useFetch = (url, config = defaultConfig) => {
+	const [data, setData] = useState(null);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		if (!url) {
+			return;
+		}
+
+		const fetchData = async () => {
+			setLoading(true);
+
+			return window
+				.fetch(url, config)
+				.then(async response => {
+					try {
+						const data = await response.json();
+
+						if (response.ok) {
+							return data;
+						} else {
+							return Promise.reject(data);
+						}
+					} catch (error) {
+						return Promise.reject(error);
+					}
+				});
+		};
+
+		fetchData()
+			.then(
+				data => setData(data),
+				error => console.error('oh no, login failed', error)
+			)
+			.finally(() => setLoading(false));
+	return {data, loading};
+};
