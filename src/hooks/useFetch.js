6ea001e66713bf2b882f82a9e0b4aa0865ab241b
Paul H Howells <p.h.howells@gmail.com>
2021-02-06 15:47:37 +0000

    Update useFetch.js
    refactor: split hook state and fetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,43 +1,58 @@
-import { useState, useEffect } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 const defaultConfig = { method: 'GET' };
 
-export const useFetch = (url, config = defaultConfig) => {
+export const fetchData = async (url, config) => {
+	console.log('REACT_APP_API_BASE_URL', process.env.REACT_APP_API_BASE_URL);
+
+	return window
+		.fetch(url, config)
+		.then(async response => {
+			try {
+				const data = await response.json();
+
+				if (response.ok) {
+					return data;
+				} else {
+					// response.statusText not in 200-299, but could be in a useful 300 range.
+					return Promise.reject(response);
+				}
+			} catch (error) {
+				return Promise.reject(new Error(error));
+			}
+		});
+};
+
+export default function useFetch({url, config = defaultConfig, interval}) {
+	const timeout = useRef();
 	const [data, setData] = useState(null);
-	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
+	const [loading, setLoading] = useState(true); // set true here?
+	const getData = useCallback(() => {
+		setLoading(true);
+
+		fetchData(url, config) // process.env.REACT_APP_API_BASE_URL + url
+			.then((data) => setData(data))
+			.catch((error) => {
+				console.error(error);
+
+				setError(error);
+			})
+			.finally(() => {
+				setLoading(false);
+
+				if (interval) {
+					clearTimeout(timeout.current);
+					timeout.current = setTimeout(() => getData(), interval);
+				}
+			});
+	}, [url, config, interval]);
 
 	useEffect(() => {
-		if (!url) {
-			return;
-		}
+		getData();
 
-		const fetchData = async () => {
-			setLoading(true);
+		return () => clearTimeout(timeout.current);
+	}, [url, config, getData]);
 
-			return window
-				.fetch(url, config)
-				.then(async response => {
-					try {
-						const data = await response.json();
-
-						if (response.ok) {
-							return data;
-						} else {
-							return Promise.reject(data);
-						}
-					} catch (error) {
-						return Promise.reject(error);
-					}
-				});
-		};
-
-		fetchData()
-			.then(
-				data => setData(data),
-				error => console.error('oh no, login failed', error)
-			)
-			.finally(() => setLoading(false));
-	}, [url, config]);
-
-	return {data, loading};
-};
+	return { data, loading, error };
+}
